b4530c869e75403b6f2c6b1728a4eba24bc1952c
Rodrigo Valle <rdan.valle@gmail.com>
2016-04-29 13:59:26 -0700

    added project 2a

--- p2a/lab2a.c
+++ p2a/lab2a.c
@@ -6,13 +6,22 @@
 #include <time.h>
 #include <pthread.h>
 
-#define FATAL 1
+#define FATAL 100
 
 static int t_count = 1;
 static int iter_count = 1;
 static int opt_yield = 0;
+static int estatus = 0;
 static long long count = 0;
 
+static pthread_mutex_t m_lock = PTHREAD_MUTEX_INITIALIZER;
+static volatile int s_lock = 0;
+
+typedef void (*add_fun_t)(long long *, long long); // typedef for add functions
+void add(long long *, long long); // add prototype
+static add_fun_t add_fun = add;   // make add() the default
+
+/* ADD FUNCTION FLAVORS */
 void
 add(long long *pointer, long long value)
 {
@@ -20,6 +29,50 @@ add(long long *pointer, long long value)
 	if (opt_yield)
 		pthread_yield();
 	*pointer = sum;
+}
+
+void
+add_mutex(long long *pointer, long long value)
+{
+	pthread_mutex_lock(&m_lock);
+	add(pointer, value);
+	pthread_mutex_unlock(&m_lock);
+}
+
+void
+add_spin_lock(long long *pointer, long long value)
+{
+	while(__sync_lock_test_and_set(&s_lock, 1))
+		;
+	add(pointer, value);
+	__sync_lock_release(&s_lock);
+}
+
+void
+add_compare_swap(long long *pointer, long long value)
+{
+	int prev, sum;
+	do {
+		prev = *pointer;
+		sum = prev + value;
+		if (opt_yield)
+			pthread_yield();
+	} while (__sync_val_compare_and_swap(pointer, prev, sum) != prev);
+}
+
+add_fun_t
+get_add_fun(char t)
+{
+	switch (t) {
+		case 'm':
+			return add_mutex;
+		case 's':
+			return add_spin_lock;
+		case 'c':
+			return add_compare_swap;
+		default: // no lock
+			return add;
+	}
 }
 
 void
@@ -35,6 +88,7 @@ parse_opts(int argc, char* argv[])
 	struct option longopts[] = {
 		{"threads"   , required_argument, NULL, 't'},
 		{"iterations", required_argument, NULL, 'i'},
+		{"sync"      , required_argument, NULL, 's'},
 		{"yield"     , no_argument, &opt_yield,  1 },
 		{0, 0, 0, 0}
 	};
@@ -46,10 +100,13 @@ parse_opts(int argc, char* argv[])
 				if (d > 0)  // these inputs better be valid (otherwise default to 1)
 					t_count = d;
 				break;
-			case 'i':;
+			case 'i':
 				d = atoi(optarg);
 				if (d > 0)
 					iter_count = d;
+				break;
+			case 's':
+				add_fun = get_add_fun(*optarg);
 				break;
 			case '?':
 				usage(argv[0]);
@@ -69,11 +126,11 @@ void
 *t_routine(__attribute__((unused)) void *arg)
 {
 	for (int i = 0; i < iter_count; i++) {
-		add(&count, 1);
+		add_fun(&count, 1);
 	}
 
 	for (int i = 0; i < iter_count; i++) {
-		add(&count, -1);
+		add_fun(&count, -1);
 	}
 	
 	return NULL;
@@ -123,11 +180,15 @@ main(int argc, char *argv[])
 			t_count, iter_count, ops);
 
 	// check for errors
-	if (count != 0)
+	if (count != 0) {
 		fprintf(stderr, "ERROR: final count = %lld\n", count);
+		estatus = 1;
+	}
 
 	// calculate the elapsed time
 	elapsed_time = time_diff(end_time, start_time);
-	printf("elapsed time: %lld\n", elapsed_time);
-	printf("per opt: %lld\n", elapsed_time/ops);
+	printf("elapsed time: %lldns\n", elapsed_time);
+	printf("per op: %lldns\n", elapsed_time/ops);
+
+	exit(estatus);
 }
