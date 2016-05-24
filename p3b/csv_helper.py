ec32592ffe1e2495f34b8443d1562ce0ef51b44d
Rodrigo Valle <rdan.valle@gmail.com>
2016-05-23 23:29:44 -0700

    csv_reader now gives a proper object describing the file. should make this project much easier to deal with

--- p3b/csv_helper.py
+++ p3b/csv_helper.py
@@ -2,69 +2,81 @@ import csv
 import os
 
 csv_fields = {
-    'super.csv': [ "magic_number",
-                   "total_inodes",
-                   "total_blocks",
-                   "block_size",
-                   "fragment_size",
-                   "blocks_per_group",
-                   "inodes_per_group",
-                   "fragments_per_group",
-                   "first_data_block" ],
+    'super.csv': [ ("magic_number", 16),
+                   ("total_inodes", 10),
+                   ("total_blocks", 10),
+                   ("block_size", 10),
+                   ("fragment_size", 10),
+                   ("blocks_per_group", 10),
+                   ("inodes_per_group", 10),
+                   ("fragments_per_group", 10),
+                   ("first_data_block", 10) ],
 
-    'group.csv': [ "blocks_in_group",
-                   "free_blocks",
-                   "free_inodes",
-                   "directories",
-                   "inode_bitmap_block",
-                   "block_bitmap_block",
-                   "inode_table_block" ],
+    'group.csv': [ ("blocks_in_group", 10),
+                   ("free_blocks", 10),
+                   ("free_inodes", 10),
+                   ("directories", 10),
+                   ("inode_bitmap_block", 16),
+                   ("block_bitmap_block", 16),
+                   ("inode_table_block", 16) ],
 
-    'bitmap.csv': [ "bitmap_block",
-                    "element_number" ],
+    'bitmap.csv': [ ("bitmap_block", 16),
+                    ("element_number", 10) ],
 
-    'inode.csv': [ "inode_number",
-                   "file_type",
-                   "mode",
-                   "owner",
-                   "group",
-                   "ref_count",
-                   "creation_time",
-                   "modification_time",
-                   "access_time",
-                   "file_size",
-                   "allocated_blocks" ],
+    'inode.csv': [ ("inode_number", 10),
+                   ("file_type", -1),
+                   ("mode", 8),
+                   ("owner", 10),
+                   ("group", 10),
+                   ("ref_count", 10),
+                   ("creation_time", 16),
+                   ("modification_time", 16),
+                   ("access_time", 16),
+                   ("file_size", 10),
+                   ("allocated_blocks", 10) ],
 
-    'directory.csv': [ "parent_inode_number",
-                       "entry_number",
-                       "entry_length",
-                       "name_length",
-                       "entry_inode",
-                       "name" ],
+    'directory.csv': [ ("parent_inode_number", 10),
+                       ("entry_number", 10),
+                       ("entry_length", 10),
+                       ("name_length", 10),
+                       ("entry_inode", 10),
+                       ("name", -1) ],
 
-    'indirect.csv': [ "containing_block",
-                      "entry_number",
-                      "block_pointer" ]
+    'indirect.csv': [ ("containing_block", 16),
+                      ("entry_number", 10),
+                      ("block_pointer", 16) ]
 }
+
+files = list(csv_fields)
 
 
 class csv_reader:
-    def __init__(self, directory):
-        self.filepaths = {f: os.path.join(directory, f) for f in list(csv_fields.keys())}
-        self.openfiles = []
+    def __init__(self, dir):
+        self.filepaths = {f: os.path.join(dir, f) for f in files}
+        self.parsedFiles = {}
 
-    def get_reader(self, type):
-        filename = type + ".csv"
+    def entry_list(self, filename):
+        if filename in self.parsedFiles:
+            return self.parsedFiles[filename]
 
-        rest = None
-        if filename == 'inode.csv':
-            rest = 'block_pointers'
+        per_line_dict = []
+        with open(self.filepaths[filename], newline='') as f:
+            for line in csv.reader(f):
+                d = {}
+                for i, value in enumerate(line):
+                    try:
+                        key, base = csv_fields[filename][i]
+                        if base > 0:  # elif <= zero, assume string
+                            value = int(value, base)
+                        d[key] = value
 
-        f = open(self.filepaths[filename], newline='')
-        self.openfiles.append(f)
-        return csv.DictReader(f, fieldnames=csv_fields[filename], restkey=rest)
+                    except IndexError:  # handle inode block pointers
+                        key = 'block_pointers'
+                        value = [int(s, 16) for s in line[i:]]
+                        d[key] = value
+                        break
 
-    def __del__(self):
-        for file in self.openfiles:
-            file.close()
-
+                per_line_dict.append(d)
+                
+        self.parsedFiles[filename] = per_line_dict
+        return per_line_dict
--- p3b/helper.py
+++ p3b/helper.py
@@ -1,12 +1,21 @@
 class inode:
-    def __init__(self, inode_nr, nlinks=0):
+    def __init__(self, inode_nr):
         self.inode_number = inode_nr
+        self.refcount = 0
         self.ptrs = []
-        self.refcount = nlinks
         self.referenced_by = []
 
+    def add_reference(self, directoryInode, entryNum):
+        self.referenced_by.append((directoryInode, entryNum))
+        self.refcount += 1
+
+
 class block:
-    def __init__(self, block_nr, nlinks=0):
+    def __init__(self, block_nr):
         self.block_number = block_nr
-        self.refcount = nlinks
+        self.refcount = 0
         self.referenced_by = []
+    
+    def add_reference(self, inode, indirectblocknum, entrynum):
+        self.referenced_by.append((inode, indirectblocknum, entrynum))
+        self.refcount += 1
